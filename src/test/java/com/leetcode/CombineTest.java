80bb3c124c82857d0f4e1211cfefa2f52dcbfcba
LiQiming <leeqiming@163.com>
2022-08-29 17:01:41 +0800

    add test

--- src/test/java/com/leetcode/CombineTest.java
+++ src/test/java/com/leetcode/CombineTest.java
@@ -1,11 +1,1 @@
-package com.leetcode;
-
-import org.junit.jupiter.api.Test;
-
-public class CombineTest {
-    @Test
-    void case_1(){
-        int[] nums = new int[]{2,3,6,7};
-        new CombineSum().combinationSum(nums, 7);
-    }
-}
+package com.leetcode;import org.junit.jupiter.api.Test;public class CombineTest {    @Test    void case_1(){        int[] nums = new int[]{2,3,6,7};        new CombineSum().combinationSum(nums, 7);    }}
