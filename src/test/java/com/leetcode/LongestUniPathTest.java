7a935cf44420f2783095d084c093b85588f49c33
LiQiming <leeqiming@163.com>
2022-09-02 10:01:25 +0800

    687. 最长同值路径

--- src/test/java/com/leetcode/LongestUniPathTest.java
+++ src/test/java/com/leetcode/LongestUniPathTest.java
@@ -1,1 +1,31 @@
-package com.leetcode;import org.junit.jupiter.api.Test;public class LongestUniPathTest {    @Test    void case_1() {        TreeNode root = new TreeNode(5);        root.left = new TreeNode(4);        root.right = new TreeNode(5);        root.left.left = new TreeNode(1);        root.left.right = new TreeNode(1);        root.right.right = new TreeNode(5);        LongestUniPath longestUniPath = new LongestUniPath();        int i = longestUniPath.longestUnivaluePath(root);        System.out.println(i);    }    @Test    void case_2() {        TreeNode root = new TreeNode(1);        root.left = new TreeNode(4);        root.right = new TreeNode(5);        root.left.left = new TreeNode(4);        root.left.right = new TreeNode(4);        root.right.right = new TreeNode(5);        LongestUniPath longestUniPath = new LongestUniPath();        int i = longestUniPath.longestUnivaluePath(root);        System.out.println(i);    }}
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class LongestUniPathTest {
+    @Test
+    void case_1() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+        LongestUniPath longestUniPath = new LongestUniPath();
+        int i = longestUniPath.longestUnivaluePath(root);
+        System.out.println(i);
+    }
+
+    @Test
+    void case_2() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        LongestUniPath longestUniPath = new LongestUniPath();
+        int i = longestUniPath.longestUnivaluePath(root);
+        System.out.println(i);
+    }
+}
