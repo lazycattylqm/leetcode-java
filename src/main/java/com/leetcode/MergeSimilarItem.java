130f12e2fd7455b14d7fe642908beb3b41e19a8c
qmli <leeqiming@163.com>
2023-02-28 10:13:30 +0800

    2363. 合并相似的物品

--- src/main/java/com/leetcode/MergeSimilarItem.java
+++ src/main/java/com/leetcode/MergeSimilarItem.java
@@ -1,26 +1,18 @@
 package com.leetcode;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MergeSimilarItem {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int[] value : items1) {
-            map.merge(value[0], value[1], Integer::sum);
-        }
-
-        for (int[] ints : items2) {
-            map.merge(ints[0], ints[1], Integer::sum);
-        }
-
-        return map.entrySet()
+        return Stream.concat(Stream.of(items1), Stream.of(items2))
+                .collect(Collectors.toMap(v -> v[0], v -> v[1], Integer::sum))
+                .entrySet()
                 .stream()
-                .map(entry -> List.of(entry.getKey(), entry.getValue()))
-                .sorted((o1, o2) -> o1.get(0) - o2.get(0))
+                .map(v -> List.of(v.getKey(), v.getValue()))
+                .sorted(Comparator.comparingInt(a -> a.get(0)))
                 .toList();
-
-
     }
 }
