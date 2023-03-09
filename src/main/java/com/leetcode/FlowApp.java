a590b0ccdd6bba61d14183a3a5139ff69e665ad9
LiQiming <leeqiming@163.com>
2023-03-09 09:55:41 +0800

    add publisher learn

---
+++ src/main/java/com/leetcode/FlowApp.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.SubmissionPublisher;
+
+public class FlowApp {
+    public static void main(String[] args) throws InterruptedException {
+        SubmissionPublisher publisher = new SubmissionPublisher<Integer>(ForkJoinPool.commonPool(), 5);
+        MySubscriber subscriber = new MySubscriber();
+        publisher.subscribe(subscriber);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " | Publishing = " + i);
+            publisher.submit(i);
+        }
+
+        publisher.close();
+        Thread.currentThread().join(1000);
+
+    }
+
+}
+
+class MySubscriber implements Flow.Subscriber<Integer>{
+
+    private Flow.Subscription subscription;
+
+    @Override public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        System.out.println("Subscriber onSubscribe");
+        subscription.request(1);
+    }
+
+    @Override public void onNext(Integer item) {
+        System.out.println(" Subscriber onNext "+item.toString());
+        subscription.request(1);
+
+    }
+
+    @Override public void onError(Throwable throwable) {
+        System.out.println("Subscriber onError");
+        throwable.printStackTrace();
+    }
+
+    @Override public void onComplete() {
+        System.out.println("Subscriber onComplete");
+
+    }
+}
+
