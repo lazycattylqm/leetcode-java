89a568a8673c0e1225fc0ee4cfc433feeb91a249
LiQiming <leeqiming@163.com>
2023-03-09 10:06:40 +0800

    add publisher learn

--- src/main/java/com/leetcode/FlowApp.java
+++ src/main/java/com/leetcode/FlowApp.java
@@ -27,23 +27,23 @@ class MySubscriber implements Flow.Subscriber<Integer>{
 
     @Override public void onSubscribe(Flow.Subscription subscription) {
         this.subscription = subscription;
-        System.out.println("Subscriber onSubscribe");
+        System.out.println(Thread.currentThread().getName() + "Subscriber onSubscribe");
         subscription.request(1);
     }
 
     @Override public void onNext(Integer item) {
-        System.out.println(" Subscriber onNext "+item.toString());
-        subscription.request(1);
+        System.out.println(Thread.currentThread().getName() + " Subscriber onNext "+item.toString());
+                subscription.request(1);
 
     }
 
     @Override public void onError(Throwable throwable) {
-        System.out.println("Subscriber onError");
+        System.out.println(Thread.currentThread().getName() + "Subscriber onError");
         throwable.printStackTrace();
     }
 
     @Override public void onComplete() {
-        System.out.println("Subscriber onComplete");
+        System.out.println(Thread.currentThread().getName() + "Subscriber onComplete");
 
     }
 }
