d8b3238dd817f125c180b3a7a9a854871c0c16da
lazycattylqm <leeqiming@163.com>
2023-08-16 12:01:35 +0800

    \2682. 找出转圈游戏输家

---
+++ src/main/java/com/leetcode/CircularGameLoser.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+import java.io.PipedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CircularGameLoser {
+    public int[] circularGameLosers(int n, int k) {
+        List<Integer> init = init(n);
+        List<Integer> cached = new ArrayList<>();
+        int round = 1;
+        runGame(init, cached, round, k, n);
+        return reject(init, cached);
+    }
+
+    private List<Integer> init(int n) {
+        return Stream.iterate(1, v -> v <= n, v -> v + 1).toList();
+    }
+
+    private void runGame(List<Integer> init, List<Integer> cached, int round, int k, int n) {
+        cached.add(1);
+        int initSeed = 0;
+        while (true) {
+            int seed = k * round;
+            int index = (initSeed + seed) % n;
+            Integer i = init.get(index);
+            if (!cached.contains(i)) {
+                cached.add(i);
+                round++;
+                initSeed=index;
+            } else {
+                return;
+            }
+        }
+
+    }
+
+    private int[] reject(List<Integer> src, List<Integer> check) {
+        return src.stream().filter(v -> !check.contains(v)).mapToInt(v -> v).toArray();
+    }
+}
