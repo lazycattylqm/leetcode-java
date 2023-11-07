bdd6e685deef01ce2bb6bac6dd4116113b099a7c
qmli <leeqiming@163.com>
2023-11-07 10:44:34 +0800

    2586. 统计范围内的元音字符串数

--- src/main/java/com/leetcode/VowelStrings.java
+++ src/main/java/com/leetcode/VowelStrings.java
@@ -2,27 +2,27 @@ package com.leetcode;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class VowelStrings {
 
-  public int vowelStrings(String[] words, int left, int right) {
-    return ((int) Arrays.stream(words)
-            .skip(left)
-            .limit(right + 1)
-            .filter(this::checkString)
-            .count());
-  }
+    public int vowelStrings(String[] words, int left, int right) {
+        return ((int) Arrays.stream(words)
+                .skip(left)
+                .limit(right + 1)
+                .filter(this::checkString)
+                .count());
+    }
 
-  private boolean checkString(String word) {
-    if (word.length()==1) {
-        return "aeiou".contains(word);
+    private boolean checkString(String word) {
+        if (word.length() == 1) {
+            return "aeiou".contains(word);
+        }
+        if (word.isEmpty()) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^[aeiou](\\w)*[aeiou]$");
+        return pattern.matcher(word)
+                .find();
     }
-    if (word.length()==0) {
-      return false;
-    }
-    Pattern pattern = Pattern.compile("^[aeiou](\\w)*[aeiou]$");
-    return pattern.matcher(word).find();
-  }
 
 }
