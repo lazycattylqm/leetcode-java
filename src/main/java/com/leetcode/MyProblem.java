3c3f5ad21de2e3ed04c7f84dc2c2a408b4aa5a6d
qmli <leeqiming@163.com>
2023-12-01 14:07:17 +0800

    gradle upgrade

--- src/main/java/com/leetcode/MyProblem.java
+++ src/main/java/com/leetcode/MyProblem.java
@@ -1,11 +1,7 @@
 package com.leetcode;
 
 public class MyProblem {
-    /**
-     * 某工厂用0，1，2代表出厂检测步骤，分三步， 0 表示第一步1 表示第二步， 2 表示第三步。需要按照0在1前执行，1在2前执行的顺序执行。
-     * 现在给出一个数组，值表示当前人员可以执行哪一步，请给出全部的组合数
-     *
-     */
+
     public int solution(int[] nums) {
         int[] dp = new int[nums.length];
         dp[0] = 1;
