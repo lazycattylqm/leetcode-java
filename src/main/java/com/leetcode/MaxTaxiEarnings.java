e916de11c7b56bca55666346f4cfde5e635bbd12
qmli <leeqiming@163.com>
2023-12-08 16:19:06 +0800

    2008 出租车最大盈利

--- src/main/java/com/leetcode/MaxTaxiEarnings.java
+++ src/main/java/com/leetcode/MaxTaxiEarnings.java
@@ -6,28 +6,29 @@ import java.util.stream.Stream;
 
 public class MaxTaxiEarnings {
     public long maxTaxiEarnings(int n, int[][] rides) {
-        List<Integer> dp = new ArrayList<>();
-        dp.add(0);
+        List<Long> dp = new ArrayList<>();
+        dp.add(0L);
         Arrays.sort(rides, Comparator.comparingInt(a -> a[1]));
         Map<Integer, List<int[]>> collect = Arrays.stream(rides)
                 .collect(Collectors.groupingBy(a -> a[1]));
         for (int i = 1; i <= n; i++) {
             dp.add(dp.get(i - 1));
+            int finalI = i;
             collect.getOrDefault(i, Collections.emptyList()).forEach(v->{
                 int start = v[0];
                 int end = v[1];
                 int tip = v[2];
                 int money = end -start + tip;
-                int lastMoney = dp.get(start);
-                int currentMoney = dp.get(end);
-                int newMoney = lastMoney + money;
+                long lastMoney = dp.get(start);
+                long currentMoney = dp.get(finalI);
+                long newMoney = lastMoney + money;
                 dp.set(end, Math.max(currentMoney, newMoney));
             });
 
         }
         return dp.stream()
-                .max(Integer::compareTo)
-                .orElse(0);
+                .max(Long::compareTo)
+                .orElse(0L);
 
     }
 
