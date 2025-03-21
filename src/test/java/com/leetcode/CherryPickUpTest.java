7cd72597881f38c49b25bc81921194dd66649b77
lazycattylqm <leeqiming@163.com>
2025-03-21 23:29:17 +0800

    update Gradle wrapper to version 8.13 and fix test assertion in CherryPickUpTest

--- src/test/java/com/leetcode/CherryPickUpTest.java
+++ src/test/java/com/leetcode/CherryPickUpTest.java
@@ -1,23 +1,17 @@
 package com.leetcode;
 
-import org.junit.jupiter.api.Test;
-
-import java.lang.invoke.VarHandle;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class CherryPickUpTest {
 
-    CherryPickUp s = new CherryPickUp();
+  CherryPickUp s = new CherryPickUp();
 
-    @Test
-    void cherryPickup() {
-        int[][] grid = {
-            {0, 1, -1},
-            {1, 0, -1},
-            {1, 1,  1}
-        };
-        int i = s.cherryPickup(grid);
-        assertSame(5, i);
-    }
+  @Test
+  void cherryPickup() {
+    int[][] grid = { { 0, 1, -1 }, { 1, 0, -1 }, { 1, 1, 1 } };
+    int i = s.cherryPickup(grid);
+    assertSame(4, i);
+  }
 }
